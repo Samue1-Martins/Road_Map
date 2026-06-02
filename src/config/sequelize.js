3077c8876152aa1f3a7c7efd6374a30ff3fcc27e
samuelDouglas <samuel30douglas@gmail.com>
2026-06-02 20:51:40 -0300

    fix: sequelize config updated

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -1,10 +1,17 @@
+import 'dotenv/config';
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize('roadmap', 'postgres', '1234', {
-    host: 'localhost',
-    dialect: 'postgres',
-    port: 5432,
-    logging: false
-});
-
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT || 'postgres',
+        port: process.env.DB_PORT || 5432,
+        logging: false,
+        define: {
+            freezeTableName: true
+        }
+    });
 export default sequelize 
